29af5ecf31f27d9dbb71f71eb6be55b94d17262b
Fausto <frasca.fausto@gmail.com>
2021-05-03 22:30:10 +0200

    minor fixes

--- Homework 2/microfun.ml
+++ Homework 2/microfun.ml
@@ -35,7 +35,7 @@ type policy_frame = state * dfa
 
 type policy_stack = policy_frame list
 
-type history_stack = symbol list
+type history_list = symbol list
 
 type value =
   | Int of int
@@ -43,13 +43,29 @@ type value =
   | RecClosure of string * string * expr * value env
 
 (* Type returned by eval *)
-type ret_val = value * policy_stack * history_stack
+type ret_val = value * policy_stack * history_list
 
 
 let rec lookup env x =
   match env with
   | []        -> failwith (x ^ " not found")
   | (y, v)::r -> if x=y then v else lookup r x
+
+
+(* 
+  Check if the dfa is well formed
+*)
+let well_formed_dfa policy =
+  let rec check_transitions transitions sigma = 
+    match transitions with
+    | [] -> true
+    | (_, sym, _)::xs -> if (List.mem sym sigma) then check_transitions xs sigma else false
+  in let rec check_accepting accepting states =
+    match accepting with
+    | [] -> true
+    | s::xs -> if (List.mem s states) then check_accepting xs states else false
+  in let check_start start states = List.mem start states
+in (check_transitions policy.transitions policy.sigma) && (check_accepting policy.accepting policy.states) && (check_start policy.start policy.states)
 
 (* 
   Transact to new state in the dfa. 
@@ -58,7 +74,9 @@ let rec lookup env x =
 let rec new_state (current_state : state) (symbol : symbol) (transitions : transition list) : state = 
   match transitions with
   | [] -> current_state
-  | (s,sym, new_s)::xs -> if (s = current_state && sym = symbol) then new_s else new_state current_state symbol xs
+  | (s,sym, new_s)::xs -> 
+    if (s = current_state && sym = symbol) then 
+    new_s else new_state current_state symbol xs
 
 (* 
   Return the state of the policy after all the transitions executed by symbols in symbol_list 
@@ -93,7 +111,7 @@ let rec check_policy_stack (policy_stack : policy_stack) (op : symbol) : policy_
         else failwith "Violated policy (check_policy_stack)"
   
 
-let rec eval (e : expr) (env : value env) (pol_stack: policy_stack) (his : history_stack) : ret_val =
+let rec eval (e : expr) (env : value env) (pol_stack: policy_stack) (his : history_list) : ret_val =
   match e with
   | CstI i -> (Int i, pol_stack, his)
   | CstB b -> (Int (if b then 1 else 0), pol_stack, his)
@@ -108,7 +126,7 @@ let rec eval (e : expr) (env : value env) (pol_stack: policy_stack) (his : histo
         | ("-", Int i1, Int i2) -> (Int (i1 -i2), pol_stack, his)
         | ("=", Int i1, Int i2) -> (Int (if i1 = i2 then 1 else 0), pol_stack, his)
         | ("<", Int i1, Int i2) -> (Int (if i1 < i2 then 1 else 0), pol_stack, his)
-        |  _ -> failwith "unknown primiRve or wrong type"
+        |  _ -> failwith "unknown primitive or wrong type"
       end
   | Let(x, eRhs, letBody) ->
       let (xVal, new_pol_stack, new_his) = eval eRhs env pol_stack his in
@@ -146,12 +164,15 @@ let rec eval (e : expr) (env : value env) (pol_stack: policy_stack) (his : histo
         | _ -> failwith "eval Call: not a function"
       end 
   | Frame(policy, body) -> 
-    let (current_state, accepted) = check_history policy his
-    in
-      if(accepted) then
-        let (value, _, new_his) = eval body env ((current_state,policy)::pol_stack) his
-        in (value, pol_stack, new_his)
-      else failwith "Violated policy (frame)"
+    if not(well_formed_dfa policy) then
+      failwith "Malformed policy"
+    else
+      let (current_state, accepted) = check_history policy his
+      in
+        if(accepted) then
+          let (value, _, new_his) = eval body env ((current_state,policy)::pol_stack) his
+          in (value, pol_stack, new_his)
+        else failwith "Violated policy (frame)"
   | Read x -> 
       let new_pol_stack = check_policy_stack pol_stack Read
       in (Int 1, new_pol_stack, his@[Read])
@@ -164,21 +185,20 @@ let rec eval (e : expr) (env : value env) (pol_stack: policy_stack) (his : histo
 
 
 (* No write *)
-let	d	:	dfa	=		
+let	no_write	:	dfa	=		
 {	states	=	[0];	
     sigma	=	[Read; Write];	
     start	=	0;	
     transitions	=	
         [
           (0,Read,0);	
-          (0,Connect,0);	
           (0,Write,1)
         ];	
     accepting	=	[0];
 }	
 
 (* No read after write *)
-let	d1	:	dfa	=		
+let	no_read_after_write	:	dfa	=		
 {	states	=	[0;1;2];	
     sigma	=	[Read; Write];	
     start	=	0;	
@@ -194,24 +214,32 @@ let	d1	:	dfa	=
     accepting	=	[0;1];
 }	
 
-
-(* Read, phi_d1[Read, phi_d[Read], Write], Write *)
-let frame1 = Frame(d, Read("prova"));;
-let r0 = Let("q", frame1, Write("x", CstI 1));;
-let r1 = Let("y",Read("x"), r0);;
-
-let frame2 = Frame(d1, r1);;
-let r2 = Let("x", Read("prova"), frame2);;
-
-let r3 = Let("z", r2, Write("x", CstI 1));;
-
-
 let h = [];;
 let e0 = [];;
 let ps = [];;
 
 
+(* Connect, no_read_after_write[Read, no_write[Read], Write], Write *)
+let frame1 = Frame(no_write, Read("prova"));;
+let r0 = Let("q", frame1, Write("x", CstI 1));;
+let r1 = Let("y",Read("x"), r0);;
+let frame2 = Frame(no_read_after_write, r1);;
+let r2 = Let("x", Connect("prova"), frame2);;
+let r3 = Let("z", r2, Write("x", CstI 1));;
+
+(* Output: (Int 1, [], [Connect; Read; Read; Write; Write]) *)
 eval r3 e0 ps h;;
+
+(* Read, no_read_after_write[Read], Write, no_read_after_write[Read] *)
+let frame1 = Frame(no_read_after_write, Read("prova"));;
+let r1 = Let("y",Read("x"), frame1);;
+let r2 = Let("x", r1, Write("x", CstI 1));;
+let frame2 = Frame(no_read_after_write, Read("x"));;
+let r3 = Let("z", r2, frame2);;
+
+(* Output: Exception: Failure "Violated policy (check_policy_stack)" *)
+eval r3 e0 ps h;;
+
 
 let iF = If(Prim("=",Var "x", CstI 0), CstI 0, Call(Var "fact", Prim("-", Var "x", CstI 1)));;
 
